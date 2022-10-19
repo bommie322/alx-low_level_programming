64166dbe6f5b717c1cc4723fc558a8f44d722b0a
Aiyende Oluwasijibomi Peter <aiyendepeter@gmail.com>
2022-10-19 09:17:32 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - This is the function of the file
+ *
+ * Return: 0 (success);
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
